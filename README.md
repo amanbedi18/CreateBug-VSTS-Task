0b3fe8234fc10ade34c870eedc4130a706c33647
Aman Bedi <amanbe@microsoft.com>
2021-04-09 03:25:04 +0530

    v2.2.1 changes

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 VSTS provides "Create work item on build failure" functionality for the Build but not Release.
 This extension emulates the same for Release by dynamically creating a bug (work item) in current or custom defined area & iteration path for the team project in VSTS on release failure with details like repro steps, errors, description, title, priority, severity & assigns it to the person who triggered the release.
 
+## Changelog v2
+
+* Task is updated to work with ADO v5.0 API's to support new dev.azure conventions and organization model to access team projects & API endpoints
+* Custom requestor is now supported
+* Added YAML multi-stage pipelines support (need additional config to enable as documented)
+* Improved telemetry logs and stability fixes
+
 ## Requirements
 
 The task requires access to OAuth token in order to get error details for a release and create a bug in VSTS.
@@ -10,11 +17,15 @@ The task requires access to OAuth token in order to get error details for a rele
 
 ![ScreenShot](images/AllowOAuth.PNG)
 
+* In case of YAML pipeline, the value of OAuth token needs to be passed as environment variable to the task as follows:
+
+![ScreenShot](images/YAMLConfig.PNG)
+
 ## How to use
 
 The task can be added at any step in the release pipeline.
 
-It can be added to the same phase(single phase pipeline) or a different phase (multi-phase pipeline). The recommended approach for either case is covered in the following steps.
+It can be added to the same phase (single phase pipeline) or a different phase (multi-phase pipeline). The recommended approach for either case is covered in the following steps.
 
 1. Add the task
 * Goto "Add Task to Agent phase" and add task from utility tab in the desired release pipeline.
@@ -22,7 +33,8 @@ It can be added to the same phase(single phase pipeline) or a different phase (m
 ![ScreenShot](images/AddTask.PNG)
 
 2. Configure the task
-* The task can be used as is without any configuration provided.
+* Enable OAuth token access as per "Requirements" section for classic OR YAML pipeline.
+* The task can then be used as is without any configuration provided.
 * The default behaviour will add the bug to default area and iteration path setting of the team project to which the release definition belongs to.
 
 ![Screenshot](images/DefaultConfig.PNG)
@@ -31,6 +43,15 @@ It can be added to the same phase(single phase pipeline) or a different phase (m
 * This will enable required settings for "Area Path" & "Iteration Path". Provide the custom paths as shown in the screenshot, the bug will be created against the same.
 
 ![ScreenShot](images/CustomConfig.PNG)
+
+* Below is an example of custom Area Path & Iteration Path in a YAML multi stage pipeline:
+
+![ScreenShot](images/CustomConfigyaml.PNG)
+
+* In case the bug created on failure needs to be assigned to a particular person, check the "Provide custom requestor" checkbox and provide the value for the custom requestor.
+* This is particularly useful when pipelines run on scheduled or trigerred by some other process in which case there is no default release requestor.
+
+![ScreenShot](images/CustomRequestor.PNG)
 
 3. Stand-alone step (single phase pipelines)
 * In a single phase release pipeline add the task after all the deployment tasks.
@@ -46,9 +67,15 @@ It can be added to the same phase(single phase pipeline) or a different phase (m
 
 ![ScreenShot](images/MultiPhase.PNG)
 
+5. Multi-Stage YAML pipeline configuration
+* In a multi-stage YAML pipeline where the  pipeline has various stages, jobs and phases, add & configure this task such that it is guaranteed to run in case any of the stage fails.
+* This can be achieved by figuring out which phase is bound to run irrespective of failures and accordingly configuring "condition" property of the task provided by YAML constructs. In below example setting it to "failed()" ensures that this task will only run when any previous task in the stage has failed. You can also pass custom flag based on previous stages to this condition but the bottomline is that the task needs to be present in one of the stages of the same release and trigger based on correct conditions. 
+
+![ScreenShot](images/YAMLSinglePhase.PNG)
+
 ## Sample Runs
 
-Below are sample runs to showcase the task for both single phase and multi phase pipelines. For demo purpose there is only one environment in the release definition but in actual practice, the task will consolidate error logs for all environments that have been executed before the task's execution and have failed phases due to errors in tasks.
+Below are sample runs to showcase the task for both single phase and multi phase pipelines. For demo purpose there is only one environment in the release definition but in actual practice, the task will consolidate error logs for all environments that have been executed before the task's execution and have failed phases due to errors in their respective tasks.
 
 ### Single Phase Release pipeline
 
@@ -96,6 +123,33 @@ Below are sample runs to showcase the task for both single phase and multi phase
 3. Finally the bug is created with environment and build details in the title, consolidated error report, severit & priority, under custom area and iteration path, assigned to the person who triggered the release.
 
 ![ScreenShot](images/MultiPhaseBug.PNG)
+
+### Multi-stage YAML pipeline
+
+1. Below is a multi stage YAML pipeline having 3 stages:
+* Build: Simulates a solution build phase with a dummy task.
+* Deploy to Dev: Simulates deployment to dev environment.
+* Deploy to QA: Simulates deployment to QA environment.
+* The "Create a bug on release failure" has been configured in Deploy to QA step for this example but ideally it should execute on all stages which are expected to fail with condition of only running when any previous task in the stage has failed (refer how to use section for YAML pipelines).
+
+![ScreenShot](images/MultiStageYAML.PNG)
+
+2. On executing a release, like before the Release fails on the "PowerShell Script" in the first phase step in deployment to QA environment stage as a result the task executes next.
+
+![ScreenShot](images/MultiStageYAMLSample.PNG)
+
+* The task will now use the custom area and iteration path for the Team Project against which the release was made.
+* Also it will assign to custom identity since "customrequestor" flag was enabled and "customrequestorid" value was set in the task config. 
+* Since "isYAML" flag was enable, the task would leverage different set of API's to get details for multi-stage YAML pipeline (if this flag was not set in YAML config, the task would use vsrm API's for classic Release definitions and error out) .
+* It scans through the release, gets all failed stages (QA in this case) upto this point, gets all failed jobs & tasks in each failed phase and errors for each such task to consolidate the same into an error report.
+* The same is written to the host in the logs window
+
+![ScreenShot](images/YAMLMultiStage.PNG)
+
+3. Finally the bug is created with multi stage pipeline name and build details in the title, consolidated error report, severit & priority, under custom area and iteration path, assigned to the custom requestor.
+
+![ScreenShot](images/YAMLMultiStageBug.PNG)
+
 
 **_TIP: Ensure that "A work item assigned notification" state is enabled (as shown below) so that the person to whom the bug is assigned can receive an email for the same._**
 
--- buildtask/CreateWorkItem.ps1
+++ buildtask/CreateWorkItem.ps1
@@ -20,7 +20,11 @@
 		team project in VSTS on release failure with 
 		details like repro steps, errors, description,
 		title, priority, severity & assigns it to the
-		person who triggered the release.
+		person who triggered the release or custom 
+		requestor in case it is provided. Supports
+		classic UI based pipelines as well as YAML
+		multi stage pipelines. Configure & customize
+		accordingly (read documentation for the same).
 		
 		Deployment steps of the script are outlined below.
 	
@@ -44,18 +48,22 @@ try {
 [string]$AuthToken = $env:SYSTEM_ACCESSTOKEN
 if($AuthToken -eq $null -or $AuthToken -eq "")
 {
-	throw "The script cannot access Personal Access Token, Please enable `"Allow scripts to access OAuth token`" flag in in Agent Phase -> Additional options."
+	throw "The script cannot access Personal Access Token, Please enable `"Allow scripts to access OAuth token`" flag in in Agent Phase -> Additional options (For classic UI based pipelines). For YAML pipelines ensure System.AccessToken is passed as environment variable to the task (check documentation)."
 }
+
+Write-Host "Starting Create Bug VSTS Task"
+Write-Host "Ensure the task is configured correctly in target YAML or classic release pipeline with correct condition and control options respectively as documented to avoid unexpected errors"
+Write-Host "Check isyamlpipeline flag in settings if this task is part of YAML pipeline as it has different API to get release details as compared to classic release pipelines & make sure System.AccessToken is passed as environment variable to the task."
 
 $Build = $env:BUILD_DEFINITIONNAME
 $ReleaseName = $env:RELEASE_RELEASENAME
-$Requestor = $env:RELEASE_REQUESTEDFOR
 $EnvironmentName = $env:RELEASE_ENVIRONMENTNAME
 [int] $ReleaseId = ( $env:RELEASE_RELEASEID -as [int])
 [string]$ReleaseDefinition = $env:RELEASE_DEFINITIONNAME
 $vstsAccount = $env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI
 $vstsUri = $env:SYSTEM_TEAMFOUNDATIONSERVERURI
 $teamProject = $env:SYSTEM_TEAMPROJECTID
+$collectionId = $env:SYSTEM_COLLECTIONID
 
 $Authentication = [Text.Encoding]::ASCII.GetBytes(":$AuthToken")
 $Authentication = [System.Convert]::ToBase64String($Authentication)
@@ -64,18 +72,33 @@ $Headers = @{
 	Authorization   = ("Basic {0}" -f $Authentication)
 }
 
+$isyaml = Get-VstsInput -Name 'isyamlpipeline' -Require
+$iscustomrequestor = Get-VstsInput -Name 'customrequestor' -Require
+
+if ($iscustomrequestor -eq $true) {
+	Write-Host "Custom Requestor provided."
+	$Requestor = Get-VstsInput -Name 'customrequestorid' -Require
+	Write-Host "Custom Requestor: $Requestor"
+}
+else {
+$Requestor = $env:RELEASE_REQUESTEDFOR
+}
+
 $customPaths = Get-VstsInput -Name 'custompaths' -Require
 if ($customPaths -eq $true) {
 	Write-Host "Custom Paths provided."
 	$AreaPath = Get-VstsInput -Name 'areapath' -Require
-	$IterationPath = Get-VstsInput -Name 'iterationpath' -Require
-	$currentIteration = $IterationPath
+	$currentIteration = Get-VstsInput -Name 'iterationpath' -Require
 	Write-Host "Custom Area Path: $AreaPath"
-	Write-Host "Custom Iteration Path: $IterationPath"
+	Write-Host "Custom Iteration Path: $currentIteration"
 }
 else {
 	Write-Host "Getting default area and iteration paths for current team project."
-	$iterationPropertiesUri = "$vstsAccount" + "DefaultCollection/" + $teamProject + "/_apis/work/TeamSettings/Iterations?$timeframe=current&api-version=4.1"
+	
+	#$iterationPropertiesUri = "$vstsAccount" + "DefaultCollection/" + $teamProject + "/_apis/work/TeamSettings/Iterations?$timeframe=current&api-version=4.1"
+	#$iterationPropertiesUri = "https://dev.azure.com/" + $collectionId + "/" +  $teamProject + "/_apis/work/teamsettings/iterations?$timeframe=current&api-version=5.0"
+	$iterationPropertiesUri = "$vstsAccount$teamProject/_apis/work/TeamSettings/Iterations?$timeframe=current&api-version=5.0"
+	
 	Write-Host "Invoking Rest Endpoint for getting default current iteration path: $iterationPropertiesUri"
 	
 	$Parameters = @{
@@ -88,7 +111,10 @@ else {
 	$currentIteration = $cIteration.path
 	Write-Host "Default current iteration Path: $currentIteration"
 	
-	$areaPathUri = "$vstsAccount" + "DefaultCollection/" + $teamProject + "/_apis/work/TeamSettings/TeamFieldValues?api-version=4.1"
+	#$areaPathUri = "$vstsAccount" + "DefaultCollection/" + $teamProject + "/_apis/work/TeamSettings/TeamFieldValues?api-version=4.1"
+	#$areaPathUri = "https://dev.azure.com/" + $collectionId + "/" +  $teamProject + "/_apis/work/teamsettings/teamfieldvalues?api-version=5.0"
+	$areaPathUri = "$vstsAccount$teamProject/_apis/work/teamsettings/teamfieldvalues?api-version=5.0"
+
 	Write-Host "Invoking Rest Endpoint for getting default area path: $areaPathUri"
 	
 	$Parameters = @{
@@ -102,71 +128,155 @@ else {
 	Write-Host "Default area path: $AreaPath"
 }
 
-$uri = "$vstsUri$teamProject/_apis/Release/releases/$($ReleaseId)?api-version=3.0-preview.1"
-Write-Host "Invoking Rest Endpoint to get current release details: $uri"
-
-$Parameters = @{
-	Uri			    = $uri
-	Method		    = 'Get'
-	Headers		    = $Headers
-}
-$result = Invoke-RestMethod @Parameters
-
 $script:errorText = "<font color = ""red""><b>The release failed due to following errors: </b></font><br/><br/>"
-$environments = $result.environments
-Write-Host "Getting Environment details"
-#Write-Output $environments
-$failedEnvironments = $environments | Where-Object { $PSItem.status -eq "rejected" -or $PSItem.status -eq "inProgress" }
-#Write-Output $failedEnvironments
 
-if ($failedEnvironments -ne $null)
-{
-	Write-Host "Getting details for the environments where the release failed."
-	foreach ($environment in $failedEnvironments)
+if ($isyaml -eq $true) {
+	Write-Host "YAML pipeline selected, getting details for multi stage pipeline configured with this task"
+	$BuildId = $env:BUILD_BuildId
+
+	$uri = "$vstsUri$teamProject/_apis/build/builds/$($BuildId)?api-version=5.0"
+	
+	Write-Host "Invoking Rest Endpoint to get current release details: $uri"
+
+	$Parameters = @{
+		Uri			    = $uri
+		Method		    = 'Get'
+		Headers		    = $Headers
+	}
+
+	$result = Invoke-RestMethod @Parameters
+
+	$script:errorText += "<font color = ""red""><b>Errors occured in yaml pipeline: </b></font><br/><br/>"
+
+	$BugTitle = "Pipeline $($result.definition.name) failed against the build $($result.buildNumber)."
+
+	$uri = "$vstsUri$teamProject/_apis/build/builds/$($BuildId)/timeline?api-version=5.1"
+	$Parameters = @{
+		Uri			    = $uri
+		Method		    = 'Get'
+		Headers		    = $Headers
+	}
+
+	Write-Host "Invoking Rest Endpoint to get current release details: $uri"
+
+	$result = Invoke-RestMethod @Parameters
+
+	Write-Host "Getting Stages details"
+	$failedStages = $result.records | Where-Object { ($PSItem.result -eq "failed" -or $PSItem.state -eq "inProgress") -and $PSItem.type -eq "stage" }
+
+	if ($failedStages -ne $null)
 	{
-		#Write-Output $environment
-		$script:errorText += "<font color = ""red""><b>Errors in environment $($environment.name) : </b></font><br/><br/>"
-		Write-Host "Getting failed phases for current environment: $($environment.name)"
-		
-		$deploymentPhases = $environment.deploySteps.releasedeployphases
-		#Write-Output $deploymentPhases
-		foreach ($phase in $deploymentPhases)
+		foreach ($failedStage in $failedStages)
 		{
-			Write-Host "Getting details of the phase: $($phase.name)"
-			#Write-Output $phase
-			$issueTasks = $phase.deploymentJobs.Tasks | Where-Object { $PSItem.issues -ne $null }
-			#Write-Output $issueTasks
-			if ($issueTasks -ne $null)
+			Write-Host "Getting details for failed stage of current environment: $($failedStage.name)"
+			$script:errorText += "<font color = ""red""><b>Errors in stage $($failedStage.name) : </b></font><br/><br/>"
+
+			$failedPhases = $result.records | Where-Object { $PSItem.parentId -eq $failedStage.id -and $PSItem.type -eq "phase" -and ($PSItem.result -eq "failed" -or $PSItem.state -eq "inProgress") }
+
+			foreach ($phase in $failedPhases)
 			{
-				Write-Host "Getting details of the tasks which failed in the current phase."			
-				foreach ($Task in $issueTasks)
+				Write-Host "Getting details of the phase: $($phase.name)"
+				$failedjobs = $result.records | Where-Object { $PSItem.parentId -eq $phase.id -and $PSItem.type -eq "job" -and ($PSItem.result -eq "failed" -or $PSItem.state -eq "inProgress") }
+
+				foreach ($job in $failedjobs)
 				{
-					Write-Host "Getting error details of the failed task: $($Task.name)"							
-					#Write-Output $Task
-					$script:errorText += "<font color = ""red"">Errors in task $($Task.name) : </font><br/><br/>"
-					$script:errorText += "<ul>"
-					Write-Host "Following errors found."	
-					
-					foreach ($issue in $Task.issues)
+					Write-Host "Getting details of the tasks which failed in the current job."
+					$failedtasks = $result.records | Where-Object { $PSItem.parentId -eq $job.id -and $PSItem.type -eq "task" -and $PSItem.result -eq "failed" }
+
+					foreach ($failedtask in $failedtasks)
 					{
-						Write-Host "Error message: $($issue.message)"																									
-						#Write-Output $issue
-						$script:errorText += "<li>"
-						$script:errorText += "$($issue.message) <br/><br/>"
-						$script:errorText += "</li>"
+						Write-Host "Getting error details of the failed task: $($failedtask.name)"
+						$script:errorText += "<font color = ""red"">Errors in task $($failedtask.name) : </font><br/><br/>"
+						$script:errorText += "<ul>"
+						Write-Host "Following errors found."
+
+						foreach ($issue in $failedtask.issues)
+						{
+							Write-Host "Error message: $($issue.message)"																									
+							$script:errorText += "<li>"
+							$script:errorText += "$($issue.message) <br/><br/>"
+							$script:errorText += "</li>"
+						}
+						$script:errorText += "</ul>"
 					}
-					$script:errorText += "</ul>"
 				}
 			}
 		}
 	}
-}
 
 Write-Host "Consolidated error report:"
 Write-Host $script:errorText
+}
+else
+{
+	Write-Host "YAML pipeline unchecked, getting details for classic release pipeline configured with this task"
+	#$uri = "$vstsUri$teamProject/_apis/Release/releases/$($ReleaseId)?api-version=3.0-preview.1"
+	#$uri = "https://vsrm.dev.azure.com/" + $collectionId + "/" +  $teamProject + "/_apis/release/releases/$($ReleaseId)?api-version=5.0"
+	$uri = "$vstsUri$teamProject/_apis/Release/releases/$($ReleaseId)?api-version=5.0"
+
+	Write-Host "Invoking Rest Endpoint to get current release details: $uri"
+
+	$Parameters = @{
+		Uri			    = $uri
+		Method		    = 'Get'
+		Headers		    = $Headers
+	}
+	$result = Invoke-RestMethod @Parameters
+
+	$environments = $result.environments
+	Write-Host "Getting Environment details"
+	$failedEnvironments = $environments | Where-Object { $PSItem.status -eq "rejected" -or $PSItem.status -eq "inProgress" }
+
+	if ($failedEnvironments -ne $null)
+	{
+		Write-Host "Getting details for the environments where the release failed."
+		foreach ($environment in $failedEnvironments)
+		{
+			$script:errorText += "<font color = ""red""><b>Errors in environment $($environment.name) : </b></font><br/><br/>"
+			Write-Host "Getting failed phases for current environment: $($environment.name)"
+			
+			$deploymentPhases = $environment.deploySteps.releasedeployphases
+			foreach ($phase in $deploymentPhases)
+			{
+				Write-Host "Getting details of the phase: $($phase.name)"
+				$issueTasks = $phase.deploymentJobs.Tasks | Where-Object { $PSItem.issues -ne $null }
+				if ($issueTasks -ne $null)
+				{
+					Write-Host "Getting details of the tasks which failed in the current phase."			
+					foreach ($Task in $issueTasks)
+					{
+						Write-Host "Getting error details of the failed task: $($Task.name)"							
+						$script:errorText += "<font color = ""red"">Errors in task $($Task.name) : </font><br/><br/>"
+						$script:errorText += "<ul>"
+						Write-Host "Following errors found."	
+						
+						foreach ($issue in $Task.issues)
+						{
+							Write-Host "Error message: $($issue.message)"																									
+							$script:errorText += "<li>"
+							$script:errorText += "$($issue.message) <br/><br/>"
+							$script:errorText += "</li>"
+						}
+						$script:errorText += "</ul>"
+					}
+				}
+			}
+		}
+	}
+
+	Write-Host "Consolidated error report:"
+	Write-Host $script:errorText
+	$BugTitle = "Release $ReleaseName failed for release definition $ReleaseDefinition in the environment $EnvironmentName against the build $Build"
+}
+
+#Uri = "$vstsAccount$teamProject/_apis/wit/workitems/`$Bug?api-version=2.2"
+#Uri = "https://dev.azure.com/" + $collectionId + "/" +  $teamProject +  "/_apis/wit/workitems/`$Bug?api-version=5.0"
+$ep = "$vstsAccount$teamProject/_apis/wit/workitems/`$Bug?api-version=5.1"
+Write-Host "Calling workitem API at following endpoint to create bug $ep"
+Write-Host "Ensure ADO project template supports bug work item type, at the minimum you'd need Agile, Scrum or CMMI processes. Basic process template does not support bug workitem type and will cause 404"
 
 $RestParams = @{
-	Uri		       = "$vstsAccount$teamProject/_apis/wit/workitems/`$Bug?api-version=2.2"
+	Uri		       = "$vstsAccount$teamProject/_apis/wit/workitems/`$Bug?api-version=5.1"
 	ContentType    = 'application/json-patch+json'
 	Headers	       = @{
 		Authorization    = ("Basic {0}" -f $authentication)
@@ -176,7 +286,7 @@ $RestParams = @{
 		@{
 			op	     = "add"
 			path	 = "/fields/System.Title"
-			value    = "Release $ReleaseName failed for release definition $ReleaseDefinition in the environment $EnvironmentName against the build $Build"
+			value    = "$BugTitle"
 		}
 		@{
 			op	     = "add"
@@ -213,15 +323,16 @@ $RestParams = @{
 Write-Host "Creating a bug with the generated error report under the configured area & iteration path with default severity & priority, populated with release details in title & assigned to the person who triggered the release."
 #$RestParams.Body
 
-try
-{
+# try
+# {
 	Invoke-RestMethod @RestParams -Verbose
-}
-catch
-{
-	$PSItem.Exception.Message
-}
+# }
+# catch
+# {
+# 	$PSItem.Exception.Message
+# }
 
+Write-Host "Bug created successully, enable Bug work items via Working with bugs setting under sprints tab to view them in taskboard. Optionally configure email in project settings on Bug creation."
 } finally {
     Trace-VstsLeavingInvocation $MyInvocation
 }
